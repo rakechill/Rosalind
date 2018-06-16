985e403cad4671b1295e488af433b4647cabb20d
Rachel Gregory <r_gregory1315@berkeley.edu>
2018-06-16 13:17:56 -0700

    mendel optimized

--- string-algorithms/mendel_inherit.py
+++ string-algorithms/mendel_inherit.py
@@ -1,5 +1,4 @@
 def mendel_inherit(k, m, n):
     total_children = sum([i for i in range(1, (k + m + n))])
-    DDxDD, DdxDd = sum([i for i in range(1, k)]), (3/4 * sum([i for i in range(1, m)]))
-    DDxdd, DDxDd, Ddxdd = (k * n), (k * m), (m * n) / 2
+    DDxDD, DdxDd, DDxdd, DDxDd, Ddxdd = sum([i for i in range(1, k)]), (3/4 * sum([i for i in range(1, m)])), (k * n), (k * m), (m * n) / 2
     return (DDxDD + DdxDd + DDxdd + DDxDd + Ddxdd) / total_children
