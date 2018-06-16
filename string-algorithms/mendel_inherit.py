32e074a208288716f6d9301c0b58cb9dfc70ee12
Rachel Gregory <r_gregory1315@berkeley.edu>
2018-06-16 13:04:44 -0700

    mendel_inherit

--- string-algorithms/mendel_inherit.py
+++ string-algorithms/mendel_inherit.py
@@ -1,16 +1,4 @@
 def mendel_inherit(k, m, n):
-    # probability of each type of parent being picked
-    # k = homo dom, m = hetero, n = homo rec
-    YY = k / sum((k, m, n))
-    Yy = m / sum((k, m, n))
-    yy = n / sum((k, m, n))
-
-    #then there are crossover probabilities
-    # YY * Yy = 100% of probability of getting both parent (which maybe is jus multiplication of 2 probabilities?)
-    # Yy * yy = 50% of probability
-    #YY * yy = 100% of prev probability
-
-    # probability of a dominant allele
-    # includes hetero AND homo dom which is confusing
-
-    return
+    total_children = sum([i for i in range(1, (k + m + n))])
+    dominant_children = sum([i for i in range(1, k)]) + (3/4 * sum([i for i in range(1, m)])) + (k * n) + (k * m) + ((m * n) / 2)
+    return dominant_children/total_children
